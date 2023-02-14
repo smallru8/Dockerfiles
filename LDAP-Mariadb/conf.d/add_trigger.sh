4c8e170cddd25bf9d9caf8dc9072dab49e43840b
smallru8 <30855604+smallru8@users.noreply.github.com>
2023-02-14 22:09:03 +0800

    Update add_trigger.sh

--- LDAP-Mariadb/conf.d/add_trigger.sh
+++ LDAP-Mariadb/conf.d/add_trigger.sh
@@ -1,3 +1,4 @@
 echo 'insert into ldap_entries (id,dn,oc_map_id,parent,keyval)values (1,"'$LDAP_BASE_DN'",3,0,1);' >> /usr/local/etc/openldap/dbschema/testdb_metadata.sql
+echo 'insert into ldap_entry_objclasses (entry_id,oc_name)values (1,"dcObject");' >> /usr/local/etc/openldap/dbschema/testdb_metadata.sql
 echo 'CREATE TRIGGER `users_after_insert` AFTER INSERT ON `users` FOR EACH ROW BEGIN INSERT INTO ldap_entries(`dn`,`keyval`) VALUES (CONCAT("cn=",NEW.`name`,"'$LDAP_BASE_DN'"),NEW.`id`); INSERT INTO wallet(`id`) VALUES (NEW.`id`); END' >> /usr/local/etc/openldap/dbschema/testdb_metadata.sql
 echo 'CREATE TRIGGER `users_before_delete` BEFORE DELETE ON `users` FOR EACH ROW BEGIN DELETE FROM `ldap_entries` WHERE `keyval`=OLD.`id`; END' >> /usr/local/etc/openldap/dbschema/testdb_metadata.sql
